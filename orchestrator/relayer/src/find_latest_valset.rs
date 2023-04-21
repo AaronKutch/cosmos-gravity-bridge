4299e6b829fb20f893dac61935940715e43e8237
Aaron Kutch <aaronkutch@att.net>
2023-04-20 21:44:33 -0500

    Revert "temporary workaround"
    This reverts commit f4816b0e4fc95e2454097b1a1e1882663b0d7af5.

--- orchestrator/relayer/src/find_latest_valset.rs
+++ orchestrator/relayer/src/find_latest_valset.rs
@@ -45,24 +45,7 @@ pub async fn find_latest_valset(
 
         // we take only the first event if we find any at all.
         if !all_valset_events.is_empty() {
-
-            let mut latest = 0;
-            let mut latest_nonce = 0;
-            let mut i = 0;
-            for event in &all_valset_events {
-                match ValsetUpdatedEvent::from_log(event) {
-                    Ok(event) => {
-                        if event.valset_nonce > latest_nonce {
-                            latest_nonce = event.valset_nonce;
-                            latest = i;
-                        }
-                    }
-                    Err(e) => panic!(),
-                }
-                i += 1;
-            }
-
-            let event = &all_valset_events[latest];
+            let event = &all_valset_events[0];
             match ValsetUpdatedEvent::from_log(event) {
                 Ok(event) => {
                     let latest_eth_valset = Valset {
